a32affb2d2f2331f38a274737ee0b9f2fb837075
Roel Plieger <roel.plieger@bnc.nl>
2018-03-20 21:48:12 +0100

    BCH added

--- start_gekko_multi.sh
+++ start_gekko_multi.sh
@@ -4,4 +4,4 @@ node gekko --config config_str.js >> /home/roel/gekko_roel/strtrade.log &
 node gekko --config config_eth.js >> /home/roel/gekko_roel/ethtrade.log &
 node gekko --config config_btc.js >> /home/roel/gekko_roel/btctrade.log &
 node gekko --config config_xmr.js >> /home/roel/gekko_roel/xmrtrade.log &
-
+node gekko --config config_bch.js >> /home/roel/gekko_roel/bchtrade.log &
